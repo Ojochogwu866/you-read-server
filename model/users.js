d26f9a825cd5e204ec5ef960a5c73301aaa70288
Ojochogwu <07afrik@gmail.com>
2023-02-08 13:24:21 +0100

    NWE

--- model/users.js
+++ model/users.js
@@ -1,22 +1,24 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
   name: {
     type: String,
     required: true,
-    min: 6,
-    max: 255,
+    minlength: 6,
+    maxlength: 255,
   },
   email: {
     type: String,
     required: true,
-    max: 255,
-    min: 6,
+    maxlength: 255,
+    minlength: 6,
   },
   password: {
     type: String,
     required: true,
-    max: 1024,
-    min: 6,
+    maxlength: 1024,
+    minlength: 6,
   },
   date: {
     type: Date,
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const Joi = require("joi");
 const User = require("../model/users");
 
+const schema = Joi.object({
+  name: Joi.string().min(6).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(6).required(),
+});
+
 router.post("/register", async (req, res) => {
-  const user = User({
+  const { error } = schema.validate(req.body, {
+    abortEarly: false,
+  });
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const user = new User({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password,
